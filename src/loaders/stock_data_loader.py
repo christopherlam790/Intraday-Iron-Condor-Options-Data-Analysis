c40d55872863e7ad1f7ac357157007aeb1ed5b42
ChrisLam790 <christopherlam790@gmail.com>
2025-12-22 13:50:57 -0500

    Docsting

--- src/loaders/stock_data_loader.py
+++ src/loaders/stock_data_loader.py
@@ -1,8 +1,12 @@
+"""
+Docstring for loaders.stock_data_loader
+
+Load processed parquete file data from the local file system.
+"""
+
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq 
-
-
 
 PARQUETE_PATH = "data/processed/underlying/hourly_stock_prices_technical_ind.parquet"
 
